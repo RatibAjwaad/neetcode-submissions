7dfc188dde4d30285a5e954017bccbcce73dba51
RatibAjwaad <ajwaad.ratib@gmail.com>
2026-06-08 21:07:49 +0600

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static boolean hasDuplicate(int[] nums) {
+        for(int i = 0; i<nums.length; i++){
+            for(int j = i + 1; j<nums.length; j++){
+                if(nums[j]==nums[i]){
+                    return true;
+                }
+                
+            }
+            
+        }
+        return false;
+        
+    }
+
+    public static void main(String[] args){
+        int [] nums = { 1,2,3,3 };
+        System.out.println(hasDuplicate(nums));
+    }
+}
